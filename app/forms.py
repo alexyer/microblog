863f5fe084783118bde10c50326cf6172c4a3538
Olexander Yermakov <mannavard1611@gmail.com>
2014-10-01 17:21:22 +0300

    Added user page and edit

--- app/forms.py
+++ app/forms.py
@@ -9,7 +9,9 @@ from flask.ext.wtf import Form
 
 from wtforms import BooleanField
 from wtforms import TextField
+from wtforms import TextAreaField
 
+from wtforms.validators import Length
 from wtforms.validators import Required
 
 
@@ -18,3 +20,7 @@ class LoginForm(Form):
     openid = TextField('openid', validators=[Required()])
     remember_me = BooleanField('remember_me', default=False)
         
+
+class EditForm(Form):
+    nickname = TextField('nickname', validators=[Required()])
+    about_me = TextAreaField('about_me', validators=[Length(min=0, max=140)])
--- app/models.py
+++ app/models.py
@@ -6,6 +6,7 @@ __email__ = 'mannavard1611@gmail.com'
 
 
 from app import db
+from hashlib import md5
 
 
 ROLE_USER = 0
@@ -18,6 +19,8 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     role = db.Column(db.SmallInteger, default=ROLE_USER)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime)
 
     def __repr__(self):
         return '<User {nick}>'.format(nick=self.nickname)
@@ -30,6 +33,9 @@ class User(db.Model):
 
     def get_id(self):
         return self.id
+
+    def avatar(self, size):
+        return 'http://www.gravatar.com/avatar/' + md5(self.email).hexdigest() + '?=mm&s=' + str(size) 
 
 
 class Post(db.Model):
